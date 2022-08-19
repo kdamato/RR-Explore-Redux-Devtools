4d82f8312e3af0c222ad349db9f9e3201820c6f0
Kyle DAmato <kyledamato4@gmail.com>
2022-08-19 14:31:27 -0400

    added nonexecutable code explaining the assignment

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counterSlice'
 import todoReducer from './features/todoSlice'
 
+//create and export store with reducers
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
